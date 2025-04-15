f845b985408ac7f5bf6afe16f929f13b2ffce3d4
NavaneethaMella <mellanavaneetha07@gmail.com>
2025-04-15 09:01:28 +0530

    Add files via upload

---
+++ app/Page3/page.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../globals.css';
+
+const GMPCompliance = () => {
+  return (
+    <div className="service-page">
+      <h1>GMP Compliance and Assessments</h1>
+      <ul>
+        <li>GMP audits and general consulting (Collaboration partner)</li>
+        <li>Quality Management System development and reviews</li>
+        <li>Validation Master Plans</li>
+        <li>Risk Assessments – Elemental Impurities, Nitrosamines etc…</li>
+      </ul>
+    </div>
+  );
+};
+
+export default GMPCompliance;
---
+++ app/Page4/page.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../globals.css';
+
+const ProductLifecycle = () => {
+  return (
+    <div className="service-page">
+      <h1>Product Lifecycle Management</h1>
+      <ul>
+        <li>Product Lifecycle strategy</li>
+        <li>Alternatives to formulation strategy and development</li>
+        <li>Regulatory Strategy</li>
+      </ul>
+    </div>
+  );
+};
+
+export default ProductLifecycle;
